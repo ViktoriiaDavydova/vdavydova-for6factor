90a7f876760bf1859dc237d7bc4c2bf23c64e838
V Davydova <44687083+HakunaMatat@users.noreply.github.com>
2021-07-11 20:14:34 -0600

    .

---
+++ src/components/RedeemItem.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    height: 200,
+    width: 800,
+    border: "none",
+    boxShadow: "none",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column"
+  },
+  content: {
+    flex: "1 0 auto",
+    width: 600,
+    textAlign: "justify"
+  },
+  cover: {
+    width: 150,
+    margin: 10
+  },
+
+  avatar: {
+    backgroundColor: "#0a84d1",
+    width: theme.spacing(3),
+    height: theme.spacing(3)
+  },
+  iconButton: {
+    padding: 10,
+    color: "#0a84d1",
+    float: "right"
+  }
+}));
+
+export default function MediaControlCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <div className={classes.details}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="recipe" className={classes.avatar}>
+              1
+            </Avatar>
+          }
+          titleTypographyProps={{ variant: "h6" }}
+          title="Delicious Cat Food"
+        />
+        <CardContent className={classes.content}>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            faucibus turpis. Turpis egestas sed tempus erat imperdiet sed
+
+          </Typography>
+          <IconButton
+            className={classes.iconButton}
+            aria-label="playCircleOutlineIcon"
+          >
+            <PlayCircleOutlineIcon />
+          </IconButton>
+        </CardContent>
+      </div>
+      <CardMedia
+        className={classes.cover}
+        image="https://images.freeimages.com/images/large-previews/d0a/cat-food-1539210.jpg"
+        title="Delicious Cat food"
+      />
+    </Card>
+  );
+}
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import UserAccount from "../UserAccount";
 import Grid from "@material-ui/core/Grid";
+import RedeemItem from "../RedeemItem";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,13 +40,13 @@ export default function MainPage() {
           </Grid>
           <Grid item xs={8} container spacing={3}>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>xs=3</Paper>
+              <RedeemItem />
             </Grid>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>xs=4</Paper>
+            <RedeemItem />
             </Grid>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>xs=5</Paper>
+            <RedeemItem />
             </Grid>
           </Grid>
         </Grid>
